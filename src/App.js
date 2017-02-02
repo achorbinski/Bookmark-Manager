5f4d730fceee9dda03a44351c925f588bb51e6dd
achorbinski <achorbinski@gmail.com>
2017-02-01 20:33:12 -0800

    updated state schema for subjects. feature not finished

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ class App extends Component {
   constructor(){
     super();
 
-    this.state = {
-      resources: [{title: "LearnCode Academy React Tutorial",
-                  url: "https://www.youtube.com/watch?v=CozSF5abcTA"},
-                  {title: "LearnCode Academy React Tutorial",
-                  url: "https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.xv9uppvae"},
-                  {title: "LearnCode Academy React Tutorial",
-                  url: "http://stack.formidable.com/es6-interactive-guide/#/"},
-                  {title: "LearnCode Academy React Tutorial",
+    this.state = {resources:
+      [
+        {subject: "Functional Programming Basics",
+        resources: [{title: "LearnCode Academy React Tutorial",
+                    url: "https://www.youtube.com/watch?v=CozSF5abcTA"},
+                    {title: "LearnCode Academy React Tutorial",
+                    url: "https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.xv9uppvae"},
+                    {title: "LearnCode Academy React Tutorial",
+                    url: "http://stack.formidable.com/es6-interactive-guide/#/"}
+                  ]},
+        {subject: "Week 2",
+        resource: [{title: "LearnCode Academy React Tutorial",
                   url: "https://www.youtube.com/watch?v=AfWYO8t7ed4"},
                   {title: "LearnCode Academy React Tutorial",
                   url: "https://www.youtube.com/watch?v=LmL0Gh193M0"},
@@ -23,9 +27,24 @@ class App extends Component {
                   {title: "Mindspace React Tutorial",
                   url: "https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"},
                   {title: "LearnCode Academy React Tutorial",
-                  url: "https://youtu.be/fd2Cayhez58"}]
-    };
+                  url: "https://youtu.be/fd2Cayhez58"}
+                ]},
+          {subject: "Week 3",
+          resource: [{title: "LearnCode Academy React Tutorial",
+                    url: "https://www.youtube.com/watch?v=AfWYO8t7ed4"},
+                    {title: "LearnCode Academy React Tutorial",
+                    url: "https://www.youtube.com/watch?v=LmL0Gh193M0"},
+                    {title: "Thinking in React",
+                    url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
+                    {title: "Mindspace React Tutorial",
+                    url: "https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"},
+                    {title: "LearnCode Academy React Tutorial",
+                    url: "https://youtu.be/fd2Cayhez58"}
+                  ]}
+          ]
   }
+};
+
   render() {
     return (
       <div>
