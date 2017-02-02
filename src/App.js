95a690cd8ba63386b2d3627fc5aad3f093b0f4ed
achorbinski <achorbinski@gmail.com>
2017-02-01 19:34:04 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -7,19 +7,13 @@ class App extends Component {
   constructor(){
     super();
 
-    this.state = {};
+    this.state = {
+      resource: "https://facebook.github.io/react/docs/thinking-in-react.html"
+    };
   }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <a href={this.state.resource}>Thinking in React</a>
     );
   }
 }
