d13205e4eac31792ab57c2c20b8db92cb76a1d60
achorbinski <achorbinski@gmail.com>
2017-03-01 19:30:56 -0800

    second commit

--- src/Components/Subject.js
+++ src/Components/Subject.js
@@ -3,12 +3,32 @@ import React, { Component } from 'react';
 export default class Subject extends Component {
   constructor(props) {
     super(props)
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      isClicked: false,
+      newResource: 'Awesome React Tutorial',
+    }
   }
 
   // event handlers
 
+  handleClick() {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }));
+  }
+
+  handleTyping(e) {
+    e.target.value
+  }
+
   render() {
     return(
+      <div>
+        <h2 onClick={this.handleClick}>{this.props.items.subject}</h2>
+      </div>
       <p>{this.props.subject}</p>
     )
   }
